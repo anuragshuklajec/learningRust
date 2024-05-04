50da145fb857dd03435669fe07152013f6b1d320
anuragshuklajec <anuragshuklajec@gmail.com>
2024-05-04 19:14:29 +0530

    Added example in error_handling to handle custom error

--- src/error_handling.rs
+++ src/error_handling.rs
@@ -6,6 +6,14 @@
 // } rust has it's own implementation of result which looks like this
 
 use std::fs;
+fn divide_num(num1 : u32, num2 : u32) -> Result<f64, String>{
+    if num2 == 0{
+        Err("can't divide number with zero".to_string())
+    }else{
+        let result = (num1 as f64) / (num2 as f64);
+        Ok(result)
+    }
+}
 fn main(){
     let res  = fs::read_to_string("example.txt");
     match res{
@@ -16,4 +24,15 @@ fn main(){
             println!("Error : {}", err);
         }
     }
+
+    let division = divide_num(1,0);
+    match division {
+        Ok(res) => {
+            println!("Division is {}", res);
+        },
+        Err(err) => {
+            println!("{}",err)
+        }
+    }
 }
+
